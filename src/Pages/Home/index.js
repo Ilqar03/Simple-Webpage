0c610dc3e942f2ceaae06eb1c78470b04d6a4284
ca r213.07 <ca.r213.07@ca.local>
2023-01-05 12:47:47 +0400

    simple

--- src/Pages/Home/index.js
+++ src/Pages/Home/index.js
@@ -17,18 +17,24 @@ function Home() {
           </section>
           <section className='write'>
             <div className='words'>
+              <div className='h'>
               <h2>Magna nostrud incididunt cupidatat cillum incididunt et nisi in ut minim reprehenderit.</h2>
               <h4>Labore irure irure laborum quis tempor aliqua. Dude</h4>
+              </div>
               <p>February 19, 2020</p>
             </div>
             <div className='words'>
+            <div className='h'>
               <h2>Dolore pariatur amet ullamco aliquip mollit aliqua qui.</h2>
               <h4>Eiusmod ipsum do dolore fugiat consequat quis non qui velit consectetur velit.</h4>
+            </div>
               <p>February 17, 2020</p>
             </div>
             <div className='words'>
+            <div className='h'>
               <h2>Velit esse dolor dolor qui cillum velit in quis exercitation occaecat.</h2>
               <h4>Adipisicing sit minim occaecat nulla proident exercitation do laboris.</h4>
+              </div>
               <p>February 15, 2020</p>
             </div>
 
